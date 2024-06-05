9d4f8e1434972dcdea5ed81c1d78b43618c6fd77
truong <ntruong@switch.tv>
2024-06-05 09:39:39 +0700

    add service worker

--- starter_project/src/client/js/urlChecker.js
+++ starter_project/src/client/js/urlChecker.js
@@ -1,13 +1,5 @@
 function checkForUrl(url) {
-  const pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      '((([a-zA-Z0-9$-_@.&+!*"(),]|%[0-9a-fA-F]{2})+(:([a-zA-Z0-9$-_@.&+!*"(),]|%[0-9a-fA-F]{2})+)?@)?' + // username:password
-      "((([a-zA-Z0-9][-a-zA-Z0-9]*[a-zA-Z0-9])\\.)*[a-zA-Z0-9][-a-zA-Z0-9]*[a-zA-Z0-9]\\.?|((\\d{1,3}\\.){3}\\d{1,3})|\\[(IPv6:[0-9a-fA-F:]+|[0-9a-fA-F:]+)])" + // hostname or IPv4/IPv6
-      "(:\\d{2,5})?" + // port
-      '(\\/([a-zA-Z0-9$-_@.&+!*"(),]|%[0-9a-fA-F]{2})*)*' + // path
-      '(\\?([a-zA-Z0-9$-_@.&+!*"(),;?]|%[0-9a-fA-F]{2})*)?' + // query string
-      '(#([a-zA-Z0-9$-_@.&+!*"(),;?]|%[0-9a-fA-F]{2})*)?$' // fragment identifier
-  );
+  const pattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
 
   return pattern.test(url);
 }
