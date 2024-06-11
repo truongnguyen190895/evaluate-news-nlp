4f44ad1bfa14a19f09254845658bf458bec06fa0
truong <ntruong@switch.tv>
2024-06-11 20:27:06 +0700

    update version

--- starter_project/webpack.dev.js
+++ starter_project/webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -26,15 +26,7 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
+    // new CleanWebpackPlugin(),
   ],
   devServer: {
     port: 3000,
